2654ab9eeb9e9e059c142e0742680d9dee5b4849
Tinadin <pitynia8717@gmail.com>
2025-04-22 06:16:50 +0300

    update styles

--- src/components/ErrorMessage/ErrorMessage.jsx
+++ src/components/ErrorMessage/ErrorMessage.jsx
@@ -2,6 +2,9 @@ import css from "./ErrorMessage.module.css";
 
 export default function ErrorMessage() {
   return (
-    <p className={css.textError}> Something's wrong... Try again, please!</p>
+    <p className={css.textError}>
+      {" "}
+      Oh no... Something's wrong... Try again, please!
+    </p>
   );
 }
